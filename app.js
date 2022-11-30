6aa4ed41e3034465369ecbb9e32d3b49fd5e4380
miklywek <miklywek@gmail.com>
2022-11-30 22:00:56 +0200

    add

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username(Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub Username!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
@@ -50,6 +66,14 @@ Add a New Project
       type: "input",
       name: "description",
       message: "Provide a description of the project (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your Project description!");
+          return false;
+        }
+      },
     },
     {
       type: "checkbox",
@@ -69,6 +93,14 @@ Add a New Project
       type: "input",
       name: "link",
       message: "Enter the GitHub link to your project. (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your Project GitHub link!");
+          return false;
+        }
+      },
     },
     {
       type: "confirm",
